6172e07646f2ba5f59e03290bcadb9d765782d05
veosotano <veosotano@gmail.com>
2021-08-25 18:17:33 +0200

    Implement processPasteInstr for rule, selector chain and simple selector

--- cpp/passes/LILPreprocessor.cpp
+++ cpp/passes/LILPreprocessor.cpp
@@ -2039,19 +2039,93 @@ bool LILPreprocessor::_processPasteInstr(std::shared_ptr<LILValuePath> value)
 
 bool LILPreprocessor::_processPasteInstr(std::shared_ptr<LILRule> value)
 {
-    std::cerr << "UNIMPLEMENTED FAIL!!!!!!!!\n\n";
+    bool hasChangesSCh = false;
+    std::vector<std::shared_ptr<LILNode>> resultNodesSCh;
+    for (auto node : value->getSelectorChains()) {
+        this->_nodeBuffer.emplace_back();
+        bool remove = this->processPasteInstr(node);
+        if (!remove && this->_nodeBuffer.back().size() == 0) {
+            resultNodesSCh.push_back(node);
+        } else {
+            hasChangesSCh = true;
+            for (auto newNode : this->_nodeBuffer.back()) {
+                resultNodesSCh.push_back(newNode);
+            }
+        }
+        this->_nodeBuffer.pop_back();
+    }
+    if (hasChangesSCh) {
+        value->setSelectorChains(std::move(resultNodesSCh));
+    }
+    
+    for (auto rule : value->getChildRules()) {
+        this->processPasteInstr(rule);
+    }
+    
+    bool hasChangesVal = false;
+    std::vector<std::shared_ptr<LILNode>> resultNodesVal;
+    for (auto node : value->getValues()) {
+        this->_nodeBuffer.emplace_back();
+        bool remove = this->processPasteInstr(node);
+        if (!remove && this->_nodeBuffer.back().size() == 0) {
+            resultNodesVal.push_back(node);
+        } else {
+            hasChangesVal = true;
+            for (auto newNode : this->_nodeBuffer.back()) {
+                resultNodesVal.push_back(newNode);
+            }
+        }
+        this->_nodeBuffer.pop_back();
+    }
+    if (hasChangesVal) {
+        value->setValues(std::move(resultNodesVal));
+    }
     return false;
 }
 
 bool LILPreprocessor::_processPasteInstr(std::shared_ptr<LILSelectorChain> value)
 {
-    std::cerr << "UNIMPLEMENTED FAIL!!!!!!!!\n\n";
+    bool hasChanges = false;
+    std::vector<std::shared_ptr<LILNode>> resultNodes;
+    for (auto node : value->getNodes()) {
+        this->_nodeBuffer.emplace_back();
+        bool remove = this->processPasteInstr(node);
+        if (!remove && this->_nodeBuffer.back().size() == 0) {
+            resultNodes.push_back(node);
+        } else {
+            hasChanges = true;
+            for (auto newNode : this->_nodeBuffer.back()) {
+                resultNodes.push_back(newNode);
+            }
+        }
+        this->_nodeBuffer.pop_back();
+    }
+    if (hasChanges) {
+        value->setNodes(std::move(resultNodes));
+    }
     return false;
 }
 
 bool LILPreprocessor::_processPasteInstr(std::shared_ptr<LILSimpleSelector> value)
 {
-    std::cerr << "UNIMPLEMENTED FAIL!!!!!!!!\n\n";
+    bool hasChanges = false;
+    std::vector<std::shared_ptr<LILNode>> resultNodes;
+    for (auto node : value->getNodes()) {
+        this->_nodeBuffer.emplace_back();
+        bool remove = this->processPasteInstr(node);
+        if (!remove && this->_nodeBuffer.back().size() == 0) {
+            resultNodes.push_back(node);
+        } else {
+            hasChanges = true;
+            for (auto newNode : this->_nodeBuffer.back()) {
+                resultNodes.push_back(newNode);
+            }
+        }
+        this->_nodeBuffer.pop_back();
+    }
+    if (hasChanges) {
+        value->setNodes(std::move(resultNodes));
+    }
     return false;
 }
 
